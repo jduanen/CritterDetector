1f6e5075d1f26dbdc08c312b52a95b9a5b3d487c
Duane Northcutt <duane.northcutt@gmail.com>
2024-11-15 12:23:35 -0800

    First cut.

--- lidar/lidarScan/lidarPlot.py
+++ lidar/lidarScan/lidarPlot.py
@@ -19,27 +19,25 @@ import pdb
 
 
 class Animator:
-    def __init__(self):
-        plt.ion()
-        self.fig, self.ax = plt.subplots(subplot_kw={'projection': 'polar'})
-        plt.show
-        self.frameNum = 0
+    def __init__(self, ax, scanner):
+        self.ax = ax
+        self.scanner = scanner
+        self.scatter = ax.plot([], [], 'ro')[0]  # red dots
 
-    def update(self, angles, distances):
-        print(f"a: {angles[0]}, {len(angles)}")  #### TMP TMP TMP
-        print(f"d: {distances[0]}, {len(distances)}")  #### TMP TMP TMP
+    def init(self):
+        self.scatter.set_data([], [])
+        return self.scatter
 
+    def update(self, frame):
         # clear the axes and replot
+        angles, distances = self.scanner.scan()
         self.ax.clear()
         self.ax.scatter(angles, distances)
-        #self.scatter.set_offsets(np.column_stack((angles, distances)))
-        #self.ax.plot(angles, distances, 'o-', label='Points')
+        ##self.ax.plot(angles, distances, 'o-', label='Points')
 
         # set rmax to be slightly larger than the max distance
         self.ax.set_ylim(0, max(max(distances), 10))
-        self.ax.set_title(f"Real-time Radial Plot (Frame {self.frameNum})")
-        plt.draw()
-        self.frameNum += 1
+        self.ax.set_title(f"Real-time Radial Plot (Frame {frame})")
 
 def getOpts():
     def signalHandler(sig, frame):
@@ -53,28 +51,23 @@ def getOpts():
             for vin in cmdQs:
                 logging.debug(f"Stopping: {vin}")
                 cmdQs[vin].put("STOP")
-    options = {'confs': {'numFrames': 10, 'delay': 1}}  #### FIXME
+    options = {'confs': {'numFrames': 10, 'delay': 100}}  #### FIXME
     return options
 
 def run(options):
     scanner = lidar.Lidar()
     if scanner:
-        animator = Animator()
-        ##pdb.set_trace()
+        fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+        animator = Animator(ax, scanner)
         numFrames = options['confs']['numFrames']
         delay = options['confs']['delay']
-        while numFrames > 0:
-            angles, distances = scanner.scan()
-            animator.update(angles, distances)
-            numFrames -= 1
-            sleep(delay)
+
+        ##pdb.set_trace()
+        ani = FuncAnimation(fig, func=animator.update, init_func=animator.init,
+                            frames=numFrames, interval=delay, blit=False)
+        plt.show()
     scanner.done()
 
-        # TODO add init_func to draw initial perimeter
-#        ani = FuncAnimation(fig, partial(update, axes=ax, device=scanner),
-#                            frames=options['confs']['numFrames'],
-#                            interval=options['confs']['delay'], repeat=False)
-#        plt.show()
 
 if __name__ == '__main__':
     opts = getOpts()
