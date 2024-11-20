eee83f5cfd9c09559bbdf60e1425b47acc18c399
Duane Northcutt <duane.northcutt@gmail.com>
2024-11-19 17:06:45 -0800

    Added detect(). Made executable.

--- lidar/lidarScan/lidarGeom.py
+++ lidar/lidarScan/lidarGeom.py
@@ -9,6 +9,8 @@ import lidar
 fig = None
 ax = None
 scanner = None
+points = None
+refArea = 0
 
 def init():
     global scanner
@@ -29,14 +31,14 @@ def polarPlot(polarCoords, color):
     global fig, ax
     angles, distances = zip(*polarCoords)
     fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
-    ax.plot(angles, distances, 'o-', label='Points', color=color)
+    ax.plot(angles, distances, 'o-', color=color)
     ax.fill(angles, distances, alpha=0.3, color=color)
 
 def cartPlot(cartCoords, color):
     global fig, ax
     x, y = zip(*cartCoords)
     fig, ax = plt.subplots()
-    ax.plot(x, y, 'o-', label='Points', color=color)
+    ax.plot(x, y, 'o-', color=color)
     ax.fill(x, y, alpha=0.3, color=color)
 
 def plotScan(polar = False, color = 'red'):
@@ -55,7 +57,7 @@ def stack():
         c = COLORS[i % len(COLORS)]
         polarCoords, cartCoords = scan()
         x, y = zip(*cartCoords)
-        ax.plot(x, y, 'o-', label='Points', color=c)
+        ax.plot(x, y, 'o-', color=c)
         ax.fill(x, y, alpha=0.3, color='gray')
 
     plt.show()
@@ -117,4 +119,70 @@ def plot(tol, num):
                         interval=(1000 / scanner.scanFreq),
                         blit=False, repeat=True)
     plt.show()
+
+def getRef(tol=0.02, num=50):
+    poly = intersect(num)
+    if tol:
+        poly = poly.simplify(tolerance=tol)
+    return poly
+
+minDiff = 10000
+maxDiff = 0
+avgDiff = 0
+
+def updateDots(frame, ax, foo):
+    global points
+    global minDiff, maxDiff, avgDiff
+    angles, distances, intensities = scanner.scanIntensity()
+    cartCoords = [polarToCartesian(theta, r) for theta, r in zip(angles, distances)]
+    poly = Polygon(cartCoords)
+    polyArea = poly.area
+    areaDiff = refArea - polyArea
+    if abs(areaDiff) < abs(minDiff):
+        minDiff = areaDiff
+    if abs(areaDiff) > abs(maxDiff):
+        maxDiff = areaDiff
+    avgDiff = (avgDiff + areaDiff) / 2
+    print(f"minDiff: {minDiff}, avgDiff: {avgDiff}, maxDiff: {maxDiff}\r", end="")
+    xy = poly.exterior.coords
+    x, y = zip(*xy)
+    if points:
+        points.remove()
+    points = ax.scatter(x, y, marker='o', c='blue')
+
+def detect(margin=-0.0025, tol=0.02, num=50):
+    global refArea
+    fig, ax = plt.subplots()
+
+    # get reference area and plot it
+    poly = getRef(tol, num)
+    if False:
+        xy = poly.exterior.coords
+        x, y = zip(*xy)
+        ax.plot(x, y, 'o-', color='red')
+        ax.fill(x, y, alpha=0.3, color='gray')
+    refPoly = poly.buffer(margin)
+    refArea = refPoly.area
+    print(f"Ref Area: {refArea}")
+    xy = refPoly.exterior.coords
+    x, y = zip(*xy)
+    ax.plot(x, y, 'o-', color='green')
+    ax.fill(x, y, alpha=0.3, color='gray')
+
+    # get scans, plot them, and compare their area
+    ani = FuncAnimation(fig, updateDots, fargs=(ax, 1), frames=1000,
+                        interval=(1000 / scanner.scanFreq),
+                        blit=False, repeat=True)
+    plt.show()
+
+
+TEST = 1
+
+
+if __name__ == '__main__':
+    init()
+    if TEST == 0:
+        plot(0.02, 50)
+    elif TEST == 1:
+        detect(-0.01, 0.02, 50)
     stop()
